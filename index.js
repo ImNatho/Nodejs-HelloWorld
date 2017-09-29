13bb6c1bd0e39d27740b78e02c61383d674bee93
Nathan Towell <me@natho.io>
2017-09-29 19:51:46 +0930

    Reformat to StandardJS

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var http = require('http');
+var http = require('http')
 
-console.log("Starting server...");
-var server = http.createServer();
+console.log('Starting server...')
+cont server = http.createServer()
 
-server.on('request', function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('HELLO WORLD!');
+server.on('request', (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write('HELLO WORLD!')
     response.end();
-});
+})
 
-server.listen(8080);
-console.log("Server running on http://127.0.0.1:8080/");
+server.listen(8080)
+console.log('Server running on http://127.0.0.1:8080/')
